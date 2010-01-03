4272a19243206699262cee2be9c68ad9bbf6f471
hubris <hubris.nemesis@gmail.com>
2010-01-03 15:37:04 +0100

    more opcodes

--- src/Cpu6502.java
+++ src/Cpu6502.java
@@ -1,4 +1,3 @@
-import java.io.Console;
 import java.lang.String;
 import java.lang.annotation.*;
 import java.lang.reflect.Constructor;
@@ -9,7 +8,7 @@ public class Cpu6502 {
 	@Target(value=ElementType.TYPE)
 	public @interface InstructionClass {}
 	
-	private enum AddressingMode { IMM, ZP, ZPX, ZPY, IZX, IZY, ABS, ABSX, ABSY, IND, REL, NONE };
+	private enum AddressingMode { IMM, ZP, ZPX, ZPY, IZX, IZY, ABS, ABSX, ABSY, IND, REL, ACC, NONE };
 	
 	private static final int STACK_MEMORY = 0x100;
 		
@@ -26,6 +25,13 @@ public class Cpu6502 {
 		boolean brk;
 		boolean overflow;
 		boolean negative;
+			
+		/**
+		 * @return all status flags in a single byte 
+		 */
+		public int getFlags() {
+			return (carry?1:0)|((zero?1:0)<<1)|((interrupt?1:0)<<2)|((decimal?1:0)<<3)|((brk?1:0)<<4)|((overflow?1:0)<<6)|((negative?1:0)<<7);			  
+		}
 	};
 	private Registers regs = new Registers();		
 	private Instruction[] instList = initInstructionList();
@@ -37,29 +43,48 @@ public class Cpu6502 {
 		protected String name;
 		protected int opcode;
 		protected int length;
+		protected AddressingMode mode;
 		protected int numCycles;
 		protected boolean extraCycle;
 
-		public Instruction(String name, int opcode, int length, int numCycles,
-				           boolean extraCycle) {
+		public Instruction(String name, int opcode, int length, AddressingMode mode, 
+				int numCycles, boolean extraCycle) {
 			super();
 			this.name = name;
 			this.opcode = opcode;
 			this.length = length;
+			this.mode = mode;
 			this.numCycles = numCycles;
 			this.extraCycle = extraCycle;
 		}		
-		abstract public void execute();
+		
+		/**
+		 * @param operand the instruction operand
+		 * @return number of cycles elapsed
+		 */
+		abstract public int execute(int operand);
 
 		public int getOpcode() {
 			return opcode;
 		}
+		
+		protected void updateNZ(int res) {
+			regs.zero = (res == 0);
+			regs.negative = (res&0x80) != 0;
+		}
+		
+		protected int convertOperand(int operand) {
+			if(mode == AddressingMode.ACC)
+				return regs.A;
+			boolean isPtr =(mode != AddressingMode.IMM) && (mode != AddressingMode.REL);				
+			return isPtr ? memory[operand] : operand;
+		}
 	}	
 
-	abstract class InstrADC extends Instruction {
-		public InstrADC(String name, int opcode, int length, int numCycles,
-		           boolean extraCycle) {
-			super(name, opcode, length, numCycles, extraCycle);
+	class InstrADC extends Instruction {
+		public InstrADC(String name, int opcode, int length, AddressingMode mode, 
+				int numCycles, boolean extraCycle) {
+			super(name, opcode, length, mode, numCycles, extraCycle);
 		}
 
 		private void bcdAdd(int operand) {
@@ -67,118 +92,476 @@ public class Cpu6502 {
 			int a = Utils6502.unpackBcd(operand);
 			int b = Utils6502.unpackBcd(regs.A);			
 			int res = a+b+carry;
-			regs.A = res%99;
+			regs.A = res%100;
 			regs.carry = res>99	;
 			regs.zero = (regs.A == 0);	
 			regs.overflow = (res>99);
-			regs.negative = (regs.A&128) != 0;
+			regs.negative = (regs.A&0x80) != 0;
 			regs.A = Utils6502.packBcd(res);		 
 		}
 		private void binaryAdd(int operand) {
 			int carry = regs.carry?1:0;
 			int res = regs.A+operand+carry;
-			regs.A = res%255;
+			regs.A = res&255;
 			regs.carry = res>255;
 			regs.zero = (regs.A == 0);	
 			regs.overflow = (res>127 || res<-128);
-			regs.negative = (regs.A&128) != 0;
+			regs.negative = (regs.A&0x80) != 0;
 		}
 		
-		protected void execute(int operand) {			
+		public int execute(int operand) {
+			operand = convertOperand(operand);
 			if(regs.decimal)
 				bcdAdd(operand);				
 			else
 				binaryAdd(operand);
-			regs.PC += length-1;			 
+			return numCycles;
 		}
 	}
 	
 	@InstructionClass class InstrADCImm extends InstrADC
 	{
 		public InstrADCImm() {
-			super("ADC", 0x69, 2, 2, false);
-		}
-		
-		public void execute() {
-			super.execute(memory[regs.PC]);
-		}		
+			super("ADC", 0x69, 2, AddressingMode.IMM, 2, false);
+		}	
 	}
 
 	@InstructionClass class InstrADCZp extends InstrADC
 	{
 		public InstrADCZp() {
-			super("ADC", 0x65, 2, 3, false);
-		}
-		
-		public void execute() {					
-			super.execute(zeroPageAddressing(memory[regs.PC]));
+			super("ADC", 0x65, 2, AddressingMode.ZP, 3, false);
 		}
 	}
 
 	@InstructionClass class InstrADCZpX extends InstrADC
 	{
 		public InstrADCZpX() {			
-			super("ADC", 0x75, 2, 4, false);
-		}
-		
-		public void execute() {
-			super.execute(zeroPageAddressingX(memory[regs.PC]));
+			super("ADC", 0x75, 2, AddressingMode.ZPX, 4, false);
 		}
 	}
 
 	@InstructionClass class InstrADCAbs extends InstrADC
 	{
 		public InstrADCAbs() {			
-			super("ADC", 0x6D, 3, 4, false);
-		}
-		
-		public void execute() {			
-			int addr = readInt(regs.PC);			
-			super.execute(memory[addr]);
+			super("ADC", 0x6D, 3, AddressingMode.ABS, 4, false);
 		}
 	}
 
 	@InstructionClass class InstrADCAbsX extends InstrADC
 	{
 		public InstrADCAbsX() {			
-			super("ADC", 0x7D, 3, 4, true);
-		}
-		
-		public void execute() {			
-			super.execute(absAddressingX(memory[regs.PC]));
+			super("ADC", 0x7D, 3, AddressingMode.ABSX, 4, true);
 		}
 	}
 
 	@InstructionClass class InstrADCAbsY extends InstrADC
 	{
 		public InstrADCAbsY() {			
-			super("ADC", 0x79, 3, 4, true);
-		}
-		
-		public void execute() {
-			super.execute(absAddressingY(memory[regs.PC]));
+			super("ADC", 0x79, 3, AddressingMode.ABSY, 4, true);
 		}
 	}
 	
 	@InstructionClass class InstrADCIndX extends InstrADC
 	{
 		public InstrADCIndX() {			
-			super("ADC", 0x61, 2, 6, false);
-		}
-		
-		public void execute() {
-			super.execute(indAddressingX(memory[regs.PC]));
+			super("ADC", 0x61, 2, AddressingMode.IZX, 6, false);
 		}
 	}
 	
 	@InstructionClass class InstrADCIndY extends InstrADC
 	{
 		public InstrADCIndY() {			
-			super("ADC", 0x71, 2, 5, true);
+			super("ADC", 0x71, 2, AddressingMode.IZY, 5, true);
+		}
+	}
+
+	class InstrAND extends Instruction {
+		public InstrAND(String name, int opcode, int length, AddressingMode mode, 
+				int numCycles, boolean extraCycle) {
+			super(name, opcode, length, mode, numCycles, extraCycle);
 		}
 		
-		public void execute() {			
-			super.execute(indAddressingY(memory[regs.PC]));
+		public int execute(int operand) {
+			operand = convertOperand(operand);			
+			regs.A = regs.A&operand;
+			updateNZ(regs.A);
+			return numCycles;
+		}
+	}
+	
+	@InstructionClass class InstrANDImm extends InstrAND
+	{
+		public InstrANDImm() {
+			super("AND", 0x29, 2, AddressingMode.IMM, 2, false);
+		}	
+	}
+
+	@InstructionClass class InstrANDZp extends InstrAND
+	{
+		public InstrANDZp() {
+			super("AND", 0x25, 2, AddressingMode.ZP, 3, false);
+		}
+	}
+
+	@InstructionClass class InstrANDZpX extends InstrAND
+	{
+		public InstrANDZpX() {			
+			super("AND", 0x35, 2, AddressingMode.ZPX, 4, false);
+		}
+	}
+
+	@InstructionClass class InstrANDAbs extends InstrAND
+	{
+		public InstrANDAbs() {			
+			super("AND", 0x2D, 3, AddressingMode.ABS, 4, false);
+		}
+	}
+
+	@InstructionClass class InstrANDAbsX extends InstrAND
+	{
+		public InstrANDAbsX() {			
+			super("AND", 0x3D, 3, AddressingMode.ABSX, 4, true);
+		}
+	}
+
+	@InstructionClass class InstrANDAbsY extends InstrAND
+	{
+		public InstrANDAbsY() {			
+			super("AND", 0x39, 3, AddressingMode.ABSY, 4, true);
+		}
+	}
+	
+	@InstructionClass class InstrANDIndX extends InstrAND
+	{
+		public InstrANDIndX() {			
+			super("AND", 0x21, 2, AddressingMode.IZX, 6, false);
+		}
+	}
+	
+	@InstructionClass class InstrANDIndY extends InstrAND
+	{
+		public InstrANDIndY() {			
+			super("AND", 0x31, 2, AddressingMode.IZY, 5, true);
+		}
+	}
+	
+	class InstrASL extends Instruction {
+		public InstrASL(String name, int opcode, int length, AddressingMode mode, 
+				int numCycles, boolean extraCycle) {
+			super(name, opcode, length, mode, numCycles, extraCycle);
+		}
+		
+		public int execute(int operand) {
+			operand = convertOperand(operand);			
+			int res = operand<<1;
+			regs.A = res&255;
+			regs.carry = (res&0x100)!=0;
+			updateNZ(regs.A);
+			return numCycles;
+		}
+	}
+
+	@InstructionClass class InstrASLAcc extends InstrASL
+	{
+		public InstrASLAcc() {
+			super("ASL", 0x0A, 1, AddressingMode.ACC, 2, false);
+		}	
+	}
+
+	@InstructionClass class InstrASLZp extends InstrASL
+	{
+		public InstrASLZp() {
+			super("ASL", 0x06, 2, AddressingMode.ZP, 5, false);
+		}
+	}
+
+	@InstructionClass class InstrASLZpX extends InstrASL
+	{
+		public InstrASLZpX() {			
+			super("ASL", 0x16, 2, AddressingMode.ZPX, 6, false);
+		}
+	}
+
+	@InstructionClass class InstrASLAbs extends InstrASL
+	{
+		public InstrASLAbs() {			
+			super("ASL", 0x0E, 3, AddressingMode.ABS, 6, false);
+		}
+	}
+
+	@InstructionClass class InstrASLAbsX extends InstrASL
+	{
+		public InstrASLAbsX() {			
+			super("ASL", 0x1E, 3, AddressingMode.ABSX, 7, false);
+		}
+	}
+
+	abstract class InstrBXX extends Instruction {
+		public InstrBXX(String name, int opcode, int length, AddressingMode mode, 
+				int numCycles, boolean extraCycle) {
+			super(name, opcode, length, mode, numCycles, extraCycle);
+		}
+		
+		abstract protected boolean mustBranch();
+		
+		public int execute(int operand) {
+			operand = convertOperand(operand);
+			int extraCycle = 0;
+			if(mustBranch()) {
+				regs.PC += operand; //If operand==0, infinite loop?
+				extraCycle++;
+			}
+			return numCycles+extraCycle;
+		}
+	}
+
+	@InstructionClass class InstrBCC extends InstrBXX {
+		public InstrBCC() {
+			super("BCC", 0x90, 2, AddressingMode.REL, 2, true);			
+		}
+		
+		protected boolean mustBranch() { return !regs.carry; }
+	}
+	
+	@InstructionClass class InstrBCS extends InstrBXX {
+		public InstrBCS() {
+			super("BCS", 0xB0, 2, AddressingMode.REL, 2, true);			
+		}
+		protected boolean mustBranch() { return regs.carry; }
+	}
+
+	@InstructionClass class InstrBEQ extends InstrBXX {
+		public InstrBEQ() {
+			super("BEQ", 0xF0, 2, AddressingMode.REL, 2, true);			
+		}
+		protected boolean mustBranch() { return regs.zero; }
+	}
+	
+	@InstructionClass class InstrBNE extends InstrBXX {
+		public InstrBNE() {
+			super("BNE", 0xD0, 2, AddressingMode.REL, 2, true);			
+		}
+		protected boolean mustBranch() { return !regs.zero; }
+	}
+
+	@InstructionClass class InstrBPL extends InstrBXX {
+		public InstrBPL() {
+			super("BPL", 0x10, 2, AddressingMode.REL, 2, true);			
+		}
+		protected boolean mustBranch() { return !regs.negative; }
+	}
+
+	@InstructionClass class InstrBMI extends InstrBXX {
+		public InstrBMI() {
+			super("BMI", 0x30, 2, AddressingMode.REL, 2, true);			
+		}
+		protected boolean mustBranch() { return regs.negative; }
+	}
+
+	@InstructionClass class InstrBVC extends InstrBXX {
+		public InstrBVC() {
+			super("BVC", 0x50, 2, AddressingMode.REL, 2, true);			
+		}
+		protected boolean mustBranch() { return !regs.overflow; }
+	}
+
+	@InstructionClass class InstrBVS extends InstrBXX {
+		public InstrBVS() {
+			super("BVS", 0x70, 2, AddressingMode.REL, 2, true);			
+		}
+		protected boolean mustBranch() { return regs.overflow; }
+	}
+
+	class InstrBIT extends Instruction {
+		public InstrBIT(String name, int opcode, int length, AddressingMode mode, 
+				int numCycles, boolean extraCycle) {
+			super(name, opcode, length, mode, numCycles, extraCycle);
+		}
+		
+		public int execute(int operand) {
+			operand = convertOperand(operand);
+			regs.negative = (operand&0x80) != 0;
+			regs.overflow = (operand&0x40) != 0;
+			int res = regs.A&operand;
+			regs.zero = (res == 0);			
+			return numCycles;
+		}
+	}
+
+	@InstructionClass class InstrBITZp extends InstrBIT
+	{
+		public InstrBITZp() {
+			super("BIT", 0x24, 2, AddressingMode.ZP, 3, false);
+		}
+	}
+
+	@InstructionClass class InstrBITAbs extends InstrBIT
+	{
+		public InstrBITAbs() {			
+			super("BIT", 0x2C, 3, AddressingMode.ABS, 4, false);
+		}
+	}
+
+	@InstructionClass class InstrBRK extends Instruction {
+		public InstrBRK() {			
+			super("BRK", 0x00, 3, AddressingMode.NONE, 7, false);
+		}
+		
+		public int execute(int operand) {
+			regs.brk = true;
+			pushInt(regs.PC);
+			pushByte(regs.getFlags());
+			regs.PC = readInt(0xFFFE);
+			return numCycles;
+		}
+	}
+
+	@InstructionClass class InstrCLC extends Instruction {
+		public InstrCLC() {			
+			super("CLC", 0x18, 1, AddressingMode.NONE, 2, false);
+		}
+		
+		public int execute(int operand) {
+			regs.carry = false;
+			return numCycles;
+		}
+	}
+
+	@InstructionClass class InstrSEC extends Instruction {
+		public InstrSEC() {			
+			super("SEC", 0x38, 1, AddressingMode.NONE, 2, false);
+		}
+		
+		public int execute(int operand) {
+			regs.carry = true;
+			return numCycles;
+		}
+	}
+
+	@InstructionClass class InstrCLI extends Instruction {
+		public InstrCLI() {			
+			super("CLI", 0x58, 1, AddressingMode.NONE, 2, false);
+		}
+		
+		public int execute(int operand) {
+			regs.interrupt = false;
+			return numCycles;
+		}
+	}
+
+	@InstructionClass class InstrSEI extends Instruction {
+		public InstrSEI() {			
+			super("SEI", 0x78, 1, AddressingMode.NONE, 2, false);
+		}
+		
+		public int execute(int operand) {
+			regs.interrupt = true;
+			return numCycles;
+		}
+	}
+
+	@InstructionClass class InstrCLV extends Instruction {
+		public InstrCLV() {			
+			super("CLV", 0xB8, 1, AddressingMode.NONE, 2, false);
+		}
+		
+		public int execute(int operand) {
+			regs.overflow = false;
+			return numCycles;
+		}
+	}
+
+	@InstructionClass class InstrCLD extends Instruction {
+		public InstrCLD() {			
+			super("CLD", 0xD8, 1, AddressingMode.NONE, 2, false);
+		}
+		
+		public int execute(int operand) {
+			regs.decimal = false;
+			return numCycles;
+		}
+	}
+
+	@InstructionClass class InstrSED extends Instruction {
+		public InstrSED() {			
+			super("SED", 0xF8, 1, AddressingMode.NONE, 2, false);
+		}
+		
+		public int execute(int operand) {
+			regs.decimal = true;
+			return numCycles;
+		}
+	}
+
+	class InstrCMP extends Instruction {
+		public InstrCMP(String name, int opcode, int length, AddressingMode mode, 
+				int numCycles, boolean extraCycle) {
+			super(name, opcode, length, mode, numCycles, extraCycle);
+		}
+		
+		public int execute(int operand) {
+			operand = convertOperand(operand);
+			int res = regs.A-operand;
+			regs.carry = (regs.A >= operand);
+			regs.zero = (regs.A == operand);
+			regs.negative = (res&0x80) == 0;
+			return numCycles;
+		}
+	}
+	
+	@InstructionClass class InstrCMPImm extends InstrCMP
+	{
+		public InstrCMPImm() {
+			super("CMP", 0xC9, 2, AddressingMode.IMM, 2, false);
+		}	
+	}
+
+	@InstructionClass class InstrCMPZp extends InstrCMP
+	{
+		public InstrCMPZp() {
+			super("CMP", 0xC5, 2, AddressingMode.ZP, 3, false);
+		}
+	}
+
+	@InstructionClass class InstrCMPZpX extends InstrCMP
+	{
+		public InstrCMPZpX() {			
+			super("CMP", 0xD5, 2, AddressingMode.ZPX, 4, false);
+		}
+	}
+
+	@InstructionClass class InstrCMPAbs extends InstrCMP
+	{
+		public InstrCMPAbs() {			
+			super("CMP", 0xCD, 3, AddressingMode.ABS, 4, false);
+		}
+	}
+
+	@InstructionClass class InstrCMPAbsX extends InstrCMP
+	{
+		public InstrCMPAbsX() {			
+			super("CMP", 0xDD, 3, AddressingMode.ABSX, 4, true);
+		}
+	}
+
+	@InstructionClass class InstrCMPAbsY extends InstrCMP
+	{
+		public InstrCMPAbsY() {			
+			super("CMP", 0xD9, 3, AddressingMode.ABSY, 4, true);
+		}
+	}
+	
+	@InstructionClass class InstrCMPIndX extends InstrCMP
+	{
+		public InstrCMPIndX() {			
+			super("CMP", 0xC1, 2, AddressingMode.IZX, 6, false);
+		}
+	}
+	
+	@InstructionClass class InstrCMPIndY extends InstrCMP
+	{
+		public InstrCMPIndY() {			
+			super("CMP", 0xD1, 2, AddressingMode.IZY, 5, true);
 		}
 	}
 
@@ -212,7 +595,7 @@ public class Cpu6502 {
 	
 	public void reset(int startAddress) {
 		regs.PC = startAddress;
-		regs.SP = STACK_MEMORY;
+		regs.SP = 0xFF;
 		regs.A = 0;
 		regs.X = 0;
 		regs.Y = 0;
@@ -229,34 +612,64 @@ public class Cpu6502 {
 	{
 		return (memory[addr]<<8)|memory[addr+1];			
 	}
+	
 	private int zeroPageAddressing(int addr) {
-		return memory[addr];
+		return addr;
 	}
 
 	private int zeroPageAddressingX(int addr) {		
-		return memory[(addr+regs.X)%255];
+		return (addr+regs.X)&255;
 	}
 
 	private int zeroPageAddressingY(int addr) {		
-		return memory[(addr+regs.Y)%255];
+		return (addr+regs.Y)&255;
+	}
+
+	private int absAddressing(int addr) {		
+		return addr;
 	}
 
 	private int absAddressingX(int addr) {		
-		return memory[addr+regs.X];
+		return addr+regs.X;
 	}
 
 	private int absAddressingY(int addr) {		
-		return memory[addr+regs.Y];
+		return addr+regs.Y;
 	}
 
 	private int indAddressingX(int addr) {		
-		int zp = (addr+regs.X)%255;
-		return memory[readInt(zp)];
+		int zp = (addr+regs.X)&255;
+		return readInt(zp);
 	}
 
 	private int indAddressingY(int addr) {		
 		int absAddr = readInt(addr)+regs.Y;	
-		return memory[absAddr];
+		return absAddr;
+	}
+	
+	private void pushInt(int val) {
+		int high = (val>>8)&0xf;
+		int low = val&0xf;
+		pushByte(high);
+		pushByte(low);
+	}
+
+	private int popInt() {
+		int low = popByte();
+		int high = popByte();
+		return (high<<8)|low;
+	}
+
+	private void pushByte(int val) {
+		int addr = regs.SP+STACK_MEMORY;
+		regs.SP = (regs.SP-1)&0xFF;
+		memory[addr] = val;
+	}
+
+	private int popByte() {		
+		regs.SP = (regs.SP+1)&0xFF;
+		int addr = regs.SP+STACK_MEMORY;
+		return memory[addr];		
 	}
 	
 	static private AddressingMode computeAddressingMode(int opcode) {
@@ -312,16 +725,67 @@ public class Cpu6502 {
 		return array;
 	}
 	
-	private static AddressingMode getAddresingMode(int opcode) {
+	private static AddressingMode getAddressingMode(int opcode) {
 		return opcodeAddressingMode[opcode];
+	}
+	
+	private int getOperand(AddressingMode mode) {
+		if(mode == AddressingMode.NONE)
+			return 0;
+		
+		int operand = memory[regs.PC++];
+		switch(mode) {
+		case IMM:
+			operand = memory[regs.PC++];
+			return operand;
+		case ZP:
+			operand = memory[regs.PC++];
+			return zeroPageAddressing(operand);
+		case ZPX:
+			operand = memory[regs.PC++];
+			return zeroPageAddressingX(operand);
+		case ZPY:
+			operand = memory[regs.PC++];
+			return zeroPageAddressingY(operand);
+		case IZX:
+			operand = memory[regs.PC++];
+			return indAddressingX(operand);
+		case IZY:
+			operand = memory[regs.PC++];
+			return indAddressingY(operand);
+		case ABS:
+			operand = readInt(regs.PC);
+			regs.PC += 2;
+			return absAddressing(operand);
+		case ABSX:
+			operand = readInt(regs.PC);
+			regs.PC += 2;
+			return absAddressingX(operand);
+		case ABSY:
+			operand = readInt(regs.PC);
+			regs.PC += 2;
+			return absAddressingY(operand);
+		case IND:
+			operand = readInt(regs.PC);
+			regs.PC += 2;
+			return readInt(operand);
+		case REL:
+			operand = memory[regs.PC++];
+			return operand;
+		default:
+			return 0;
+		}
 	}
 	
 	public void run() {
 		for(;;) {
 			int opcode = memory[regs.PC++];
+			AddressingMode mode = getAddressingMode(opcode);			
+			int operand = getOperand(mode);
 			Instruction inst = instList[opcode];
-			if(inst != null)
-				inst.execute();
+			if(inst != null) {
+				ticks += inst.execute(operand);				
+			}
 			else
 				System.err.printf("Unknown opcode %x\n", opcode);
 		}
